50b69df3f557e960b69eff01ce7dbbfd008d8458
brownies <87923078+BrowniesApp@users.noreply.github.com>
2024-02-23 01:54:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ const shifMessage = () => {
     const wordArray = [...inputOriginal.value.toUpperCase()];
     printChar(0, wordArray);
 }
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
 
 const printChar = (currentLetterIndex, wordArray) => {
     if(wordArray.length === currentLetterIndex) return;
