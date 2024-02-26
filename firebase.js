f1b6b2cdb7e127a35069a63d15859ecaa8d57ee3
brownies <87923078+BrowniesApp@users.noreply.github.com>
2024-02-26 05:31:24 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
 import { auth } from "./firebase.js";
-import { showMessage } from "./showMessage.js";
+import { showMessage } from "./src/app/showMessage.js";
 
 const signUpForm = document.querySelector("#signup-form");
 
